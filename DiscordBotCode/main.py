6a6fb05856913bffd3b3159d692ff367f457bcfc
Augustine <waugustine@mtech.edu>
2022-10-17 10:50:52 -0600

    added method to get token from .env file and added code so bot says hello back

--- DiscordBotCode/main.py
+++ DiscordBotCode/main.py
@@ -1,2 +1,17 @@
 import discord
-import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith('hello'):
+        await message.channel.send('Hello! I am a bot.')
+
+client.run(TOKEN)
