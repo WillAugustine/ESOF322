c96ee3f03dbfec61b740f73f85ccad72e748ab08
Will Augustine <william_augustine@hotmail.com>
2022-10-24 10:46:27 -0600

    added files and Discord bot now runs in server instead of only DMs. Changed from client to bot

--- DiscordBotCode/main.py
+++ DiscordBotCode/main.py
@@ -1,35 +1,38 @@
+import string
 import discord
 import os
 from dotenv import load_dotenv
 from random import randint
+from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client(intents=discord.Intents.default())
+# prefix for messages intended for the bot
+bot = commands.Bot(intents=discord.Intents.all(), command_prefix='!')
 
-@client.event
+
+brooklyn_99_quotes = [
+    'I\'m the human form of the 💯 emoji.',
+    'Bingpot!',
+    'Cool. Cool cool cool cool cool cool cool,',
+    'no doubt no doubt no doubt no doubt.'
+]
+
+@bot.command(name="99", help='Responds with a random quote from Brooklyn 99')
+async def nine_nine(ctx):
+    response = brooklyn_99_quotes[randint(0,3)]
+    await ctx.send(response)
+
+# takes input after '!happy' command and spits it out in celebratory message
+@bot.command(name="happy", help='Responds with a celebratory message')
+async def happy_birthday(ctx,*,message):
+    await ctx.send(f'Happy {message}! 🎈🎉')
+
+
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f'{bot.user} has connected to Discord!')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    brooklyn_99_quotes = [
-        'I\'m the human form of the 💯 emoji.',
-        'Bingpot!',
-        'Cool. Cool cool cool cool cool cool cool,',
-        'no doubt no doubt no doubt no doubt.'
-    ]
-
-    if '99' in message.content.lower():
-        response = brooklyn_99_quotes[randint(0,3)]
-        await message.channel.send(response)
-        
-    elif 'happy birthday' in message.content.lower():
-        await message.channel.send('Happy Birthday! 🎈🎉')
-
-
-client.run(TOKEN)
+bot.run(TOKEN)
---
+++ DiscordBotCode/trivia.py
@@ -0,0 +1,4 @@
+from trivia import trivia
+import asyncio
+
+print(trivia.question(amount=1, category=0))
